57882f6f07612b6c6401d6c039dd938991a00039
sunnywpnk1 <wichayaporn9988@gmail.com>
2025-10-01 13:31:09 +0700

    add todomodel

--- firebase/TodosModel.js
+++ firebase/TodosModel.js
@@ -6,6 +6,19 @@ import {
 
 // เป็น pointer ที่คอยควบการทำงานที่เห็นภาพทั้งหมดใน database
 const db = getFirestore(todosApp)
-//
 const todosColl = collection(db,'todos') 
 const userColl = collection(db,'users')
+
+//ดึงข้อมูลทั้งหมดใน todo แบบไม่มีเงื่อนไข ,จะมี async/await
+export const getAllTodos = async(success,unsuccess) => {
+    console.log(`getAllTodos active`)
+    try{
+        const qry = query(todosColl) //todosColl ทำหน้าที่คล้าย pointer
+        const qrySnapshot = await getDocs(qry)
+        //loop all item
+        
+    }catch(e){
+        unsuccess()
+    }
+    
+}
